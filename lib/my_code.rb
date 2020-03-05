ea40df12822828bd6394d4c0fb6b117f642df3bd
Maharaj Syed <maharajsyed@yahoo.com>
2020-03-05 14:58:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
-# Your Code Here
+#Your Code Here
+
+#My own map:
+
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length
+    new_array.push(yield(array[i]))
+    i += 1 
+  end
+  new_array
+end
+
+
+#My own reduce:
+
+def reduce (array, sv=nil)
+  if sv
+    sum = sv
+    i =0 
+  else
+    sum = array[0]
+    i =1 
+  end
+    while i<array.length
+      sum = yeild(sum, array[1])
+      i += 1 
+    end
+    sum
+  end
+  
